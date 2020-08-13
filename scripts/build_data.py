0426a43da7092585fe1d39ce7e45ceb8f1a4df95
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2020-08-13 18:15:43 +0100

    Update build_data.py

--- scripts/build_data.py
+++ scripts/build_data.py
@@ -4,7 +4,7 @@ def alter_meta(meta):
     for i in range(1,5):
         if "source_active_"+str(i) in meta:
             if meta["source_active_"+str(i)] == "true":
-                meta["source_active_"+str(i)] = 'custom.true'
+                meta["source_active_"+str(i)] = 'custom.True'
     return meta
 
 open_sdg_build(config='config_data.yml', alter_meta=alter_meta)
