539cb7ccb4f6b8fc06a4075a01b0540a65e096e5
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2020-08-13 17:57:07 +0100

    Update check_data.py

--- scripts/check_data.py
+++ scripts/check_data.py
@@ -3,9 +3,9 @@ from sdg.open_sdg import open_sdg_check
 
 def alter_meta(meta):
     for i in range(1,5):
-        if "source_active_"+i in meta:
-            if meta["source_active_"+i] == "true":
-                meta["source_active_"+i] = 'custom.true'
+        if "source_active_"+str(i) in meta:
+            if meta["source_active_"+str(i)] == "true":
+                meta["source_active_"+str(i)] = 'custom.true'
     return meta
 
 # Validate the indicators.
