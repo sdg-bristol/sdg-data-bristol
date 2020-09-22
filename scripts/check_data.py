3fbe3b8cfc6eec2485750364887e25470afa0bda
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2020-09-22 10:56:26 +0100

    Update check_data.py

--- scripts/check_data.py
+++ scripts/check_data.py
@@ -5,6 +5,14 @@ def alter_meta(meta):
     for i in range(1,5):
         if "source_organisation_"+str(i) in meta:
             meta["source_active_"+str(i)] = 'custom.boolean_true'
+    if meta["data_non_statistical"] == "Y":
+        meta["data_non_statistical"] == 'custom.boolean_true'
+    else:
+        meta["data_non_statistical"] == 'custom.boolean_false'
+    if meta["data_show_map"] == "Y":
+        meta["data_non_statistical"] == 'custom.boolean_true'
+    else:
+        meta["data_show_map"] == 'custom.boolean_false'
     return meta
 
 # Validate the indicators.
