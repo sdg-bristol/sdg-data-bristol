57d6346fa092e28477e3300059ecbe70100f6c23
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2020-09-22 11:11:20 +0100

    Update build_data.py

--- scripts/build_data.py
+++ scripts/build_data.py
@@ -5,14 +5,17 @@ def alter_meta(meta):
     for i in range(1,5):
         if "source_organisation_"+str(i) in meta:
             meta["source_active_"+str(i)] = True
-    if meta["data_non_statistical"] == "Y":
-        meta["data_non_statistical"] == True
+    if "data_non_statistical" in meta:
+        if meta["data_non_statistical"] == "Y":
+            meta["data_non_statistical"] == True
     else:
         meta["data_non_statistical"] == False
-    if meta["data_show_map"] == "Y":
-        meta["data_non_statistical"] == True
+    if "data_show_map" in meta:
+        if meta["data_show_map"] == "Y":
+            meta["data_non_statistical"] == True
     else:
         meta["data_show_map"] == False
     return meta
+
 
 open_sdg_build(config='config_data.yml', alter_meta=alter_meta)
