c4c9f469abbb67390f112ddf37b998f40faec51c
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2020-09-22 11:09:30 +0100

    Update build_data.py

--- scripts/build_data.py
+++ scripts/build_data.py
@@ -1,17 +1,18 @@
 from sdg.open_sdg import open_sdg_build
 
 def alter_meta(meta):
+    print(meta)
     for i in range(1,5):
         if "source_organisation_"+str(i) in meta:
-            meta["source_active_"+str(i)] = 'custom.boolean_true'
+            meta["source_active_"+str(i)] = True
     if meta["data_non_statistical"] == "Y":
-        meta["data_non_statistical"] == 'custom.boolean_true'
+        meta["data_non_statistical"] == True
     else:
-        meta["data_non_statistical"] == 'custom.boolean_false'
+        meta["data_non_statistical"] == False
     if meta["data_show_map"] == "Y":
-        meta["data_non_statistical"] == 'custom.boolean_true'
+        meta["data_non_statistical"] == True
     else:
-        meta["data_show_map"] == 'custom.boolean_false'
+        meta["data_show_map"] == False
     return meta
 
 open_sdg_build(config='config_data.yml', alter_meta=alter_meta)
